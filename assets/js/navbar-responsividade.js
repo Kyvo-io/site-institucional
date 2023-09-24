727b8025623b87744fdf9e172028a90dc67d743f
Carlos Goes <125396028+carlosgoes0@users.noreply.github.com>
2023-09-24 19:15:03 -0300

    Prototipo em HTML

--- assets/js/navbar-responsividade.js
+++ assets/js/navbar-responsividade.js
@@ -6,3 +6,31 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-el a");
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault(); // Impedir o comportamento padrão do link
+
+            const targetId = link.getAttribute("href").substring(1);
+
+            // Ocultar todas as seções de conteúdo
+            const sections = document.querySelectorAll(".containerHome, .containerSobreNos, .containerIntegrantes, .containerSolucao, .containerContato");
+            sections.forEach((section) => {
+                section.style.display = "none";
+            });
+
+            // Exibir a seção correspondente
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                targetSection.style.display = "flex";
+
+                
+            }
+        });
+    });
+});
