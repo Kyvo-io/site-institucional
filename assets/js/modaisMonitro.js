993de7c8a945b3bfb34251f664632e419dceb9e7
carlosgoes0 <carlos.goes@sptech.school>
2023-10-10 14:06:44 -0300

    Inicio dos modais e navegação dash

---
+++ assets/js/modaisMonitro.js
@@ -0,0 +1,15 @@
+const fundo = document.getElementById("fundo_modal")
+const modalDashEspecifica = document.getElementById("ModalEspecifica")
+const modalDashDetalhes = document.getElementById("ModalDetalhes")
+
+function trocarExibicaoModalEspecifica() {
+    modalDashEspecifica.classList.toggle("exibir")
+}
+
+function trocarExibicaoModalDetalhes() {
+    modalDashDetalhes.classList.toggle("exibir")
+}
+
+
+
+
